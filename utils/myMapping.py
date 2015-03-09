54c63919852170fd9e562525251a37189a0a0a3e
Lucas Tittmann <tittmann@ldogmac02.ens.fr>
2015-03-09 15:53:34 +0100

    update

--- utils/myMapping.py
+++ utils/myMapping.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # LibsDyogen
 # python 2.7
-# Copyright © 2013 IBENS/Dyogen Joseph LUCAS, Matthieu MUFFATO and Hugues ROEST CROLLIUS
+# Copyright © 2013 IBENS/Dyogen Joseph LUCAS, Matthieu MUFFATO, Lucas TITTMANN and Hugues ROEST CROLLIUS
 # mail : hrc@ens.fr or jlucas@ens.fr
 # This is free software, you may copy, modify and/or distribute this work under the terms of the GNU General Public License, version 3 (GPL v3) or later and the CeCiLL v2 license in France
 
